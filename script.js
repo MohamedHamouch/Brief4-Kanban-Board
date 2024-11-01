da448bc59c7f4800a7e81c64f9800200ead4f227
MohamedHamouch <med10hamouch@gmail.com>
2024-11-01 15:49:15 +0100

    complete adding function

--- script.js
+++ script.js
@@ -21,29 +21,41 @@ form.addEventListener('submit', (event) => {
     const cardTitle = document.createElement("p");
     cardTitle.textContent = taskTitle.value;
     cardTitle.className = "bg-green-600 text-white text-lg text-center";
+    card.appendChild(cardTitle);
 
     const cardDescription = document.createElement("p");
     cardDescription.textContent = taskDescription.value;
     cardDescription.className = "min-h-20 w-full pt-6";
+    card.appendChild(cardDescription);
+
 
     // creating buttons and date inside container 
-    const cardBottom = document.creatElement("div");
-    cardBottom.className = "flex items-center min-h-16 pt-6 w-full"
+    const cardBottom = document.createElement("div");
+    cardBottom.className = "flex items-center min-h-16 pt-6 w-full";
 
-    const editBtn = document.createElement("button")
-    editBtn.type = "button"
-    editBtn.textContent = "Edit"
-    editBtn.className = "h-9 w-16 mx-3 text-white text-sm font-medium bg-sky-500 rounded-md shadow shadow-teal-950 hover:opacity-75"
+    const editBtn = document.createElement("button");
+    editBtn.type = "button";
+    editBtn.textContent = "Edit";
+    editBtn.className = "h-9 w-16 mx-3 text-white text-sm font-medium bg-sky-500 rounded-md shadow shadow-teal-950 hover:opacity-75";
 
-    const deleteBtn = document.createElement("button")
-    editBtn.type = "button"
-    editBtn.textContent = "Delete"
-    editBtn.className = "h-9 w-16 mx-2 text-white text-sm font-medium bg-orange-600 rounded-md shadow shadow-teal-950 hover:opacity-75"
-    cardBottom.appendChilde()
+    const deleteBtn = document.createElement("button");
+    deleteBtn.type = "button";
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "h-9 w-16 mx-2 text-white text-sm font-medium bg-orange-600 rounded-md shadow shadow-teal-950 hover:opacity-75";
 
+    const cardDeadline = document.createElement("p");
+    cardDeadline.textContent = taskDeadline.value;
+    cardDeadline.className = "text-sm font-medium mx-6";
 
+    cardBottom.appendChild(editBtn);
+    cardBottom.appendChild(deleteBtn);
+    cardBottom.appendChild(cardDeadline);
+    card.appendChild(cardBottom);   
 
-    console.log(cardTitle)
-    
-    
+    document.querySelector("#todoContainer").appendChild(card);
+
+    //make delete button workd
+
+    deleteBtn.addEventListener("click" )
+
 });
